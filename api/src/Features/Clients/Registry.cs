80d0076021db5645c314d73b46414ea71a3a9af7
Jhaveri-Jeet <jeetwork941@gmail.com>
2025-03-21 06:28:22 +0530

    registry file changed and added all the api requirements

--- api/src/Features/Clients/Registry.cs
+++ api/src/Features/Clients/Registry.cs
@@ -8,14 +8,29 @@ public class Registry : IRegistry
 {
     public void MapEndpoints(WebApplication app)
     {
+        app.MapGet("/clients", GetAll.Handler.HandleAsync);
+        app.MapGet("/client/{clientId}", GetById.Handler.HandleAsync);
+
         app.MapPost("/client", Add.Handler.AddAsync);
+
+        app.MapPut("/client/{clientId}", Update.Handler.HandleAsync);
     }
 
     public void AddServices(IServiceCollection services)
     {
+        services.AddScoped<GetAll.IRepository, GetAll.Repository>();
+        services.AddScoped<GetAll.Service>();
+
+        services.AddScoped<GetById.IRepository, GetById.Repository>();
+        services.AddScoped<GetById.Service>();
+
         services.AddScoped<IValidator<Add.Request>, Add.RequestValidator>();
         services.AddScoped<Add.Service>();
         services.AddScoped<Add.IRepository, Add.Repository>();
+
+        services.AddScoped<IValidator<Update.Request>, Update.RequestValidator>();
+        services.AddScoped<Update.Service>();
+        services.AddScoped<Update.IRepository, Update.Repository>();
     }
 
 }
